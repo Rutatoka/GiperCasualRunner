b9ce9b5bac8bfba61fa9b89e9767f3581df0b240
Angelina <85025553+Rutatoka@users.noreply.github.com>
2026-05-06 17:20:14 +0200

    lastchanse

--- Assets/Scripts/Managers/MenuUIController.cs
+++ Assets/Scripts/Managers/MenuUIController.cs
@@ -5,16 +5,23 @@ using UnityEngine.UI;
 
 public class MenuUIController : MonoBehaviour
 {
-    public Button buttonStart;
+     Button buttonStart;
+     Button buttonExit;
 
     public void Start()
     {
-        buttonStart = GetComponent<Button>();
+        buttonStart = GameObject.Find("StartButton").GetComponent<Button>();
+        buttonExit = GameObject.Find("ExitButton").GetComponent<Button>();
         buttonStart.onClick.AddListener(OnStartButtonClicked);
+        buttonExit.onClick.AddListener(OnExittButtonClicked);
     }
 
     private void OnStartButtonClicked()
     {
         GameManager.Instance.StartGame();
+    }
+    private void OnExittButtonClicked()
+    {
+        Application.Quit();
     }
 }
